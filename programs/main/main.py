6e28f758546f878d44e35a63990e240c4f837b0e
starlight_0208 <2682994272@qq.com>
2024-08-11 12:05:18 +0800

    fix some bugs

--- programs/main/main.py
+++ programs/main/main.py
@@ -10,6 +10,8 @@ RES_DIR = f'{os.getcwd()}\\data'
 useRawMode = False
 rate_limit = False
 interval = 1
+retry: bool = False
+retry_count:int = 3
 
 # This is the filename list of Github-Ranking Markdown files
 langs = ["ActionScript", "C", "CPP", "CSS", "CSharp", 
@@ -45,10 +47,14 @@ getData = lambda baseUrl: process_func(requests.get(baseUrl, timeout=10).text)
 def nativeMode():
     for i in langs:
         start_time = time.perf_counter()
-        baseUrl = f"https://github.com/EvanLi/Github-Ranking/raw/master/Top100/{i}.md"
-        ps = getData(baseUrl)
         print(f"Processing Language: {i}".ljust(60), end="")
-        pandas.DataFrame(ps).to_csv(f'{RES_DIR}\\csv\\{i.lower}.csv')
+        baseUrl = f"https://github.com/EvanLi/Github-Ranking/raw/master/Top100/{i}.md"
+        try:
+            ps = getData(baseUrl)
+        except Exception:
+            print("[FAILED]")
+            continue
+        pandas.DataFrame(ps).to_csv(f'{RES_DIR}\\csv\\{i.lower}.csv', index=False)
         with open(f"{RES_DIR}\\json\\{i.lower()}.json", 'w', encoding='utf-8') as f:
             json.dump(ps, f, ensure_ascii=False)
             f.close()
@@ -56,57 +62,82 @@ def nativeMode():
         print(f"[OK] {end_time - start_time:.4f} seconds")
         if (rate_limit): time.sleep(interval)
     
-    print(f"Processing Language: {i}".ljust(60), end="")
+    print(f"Processing Language: Top 100 Stars".ljust(60), end="")
     start_time = time.perf_counter()
     baseUrl = f"https://github.com/EvanLi/Github-Ranking/raw/master/Top100/Top-100-stars.md"
-    TopStars = pandas.DataFrame(baseUrl)
+    try:
+        ps = getData(baseUrl)
+    except Exception as e:
+        print("[FAILED]")
+        ps = []
+    TopStars = pandas.DataFrame(ps)
     with open(f"{RES_DIR}\\json\\top-stars.json", 'w', encoding='utf-8') as f:
         json.dump(TopStars, f, ensure_ascii=False)
         f.close()
     end_time = time.perf_counter()
-    print(f"[OK] {end_time - start_time:.4f} seconds")
+    if ps != []: print(f"[OK] {end_time - start_time:.4f} seconds")
 
-    print(f"Processing Language: {i}".ljust(60), end="")
+    print(f"Processing Language: Top 100 Forks".ljust(60), end="")
     start_time = time.perf_counter()
     baseUrl = f"https://github.com/EvanLi/Github-Ranking/raw/master/Top100/Top-100-forks.md"
-    TopForks = pandas.DataFrame(baseUrl)
+    try:
+        ps = getData(baseUrl)
+    except Exception as e:
+        print("[FAILED]")
+        ps = []
+    TopForks = pandas.DataFrame(ps)
     with open(f"{RES_DIR}\\json\\top-forks.json", 'w', encoding='utf-8') as f:
         json.dump(TopForks, f, ensure_ascii=False)
         f.close()
     end_time = time.perf_counter()
-    print(f"[OK] {end_time - start_time:.4f} seconds")
+    if ps != []: print(f"[OK] {end_time - start_time:.4f} seconds")
 
 def dictMode(): 
     resultSet = {}
     for i in langs:
-        baseUrl = f"https://github.com/EvanLi/Github-Ranking/raw/master/Top100/{i}.md"
-        ps = getData(baseUrl)
+        start_time = time.perf_counter()
         print(f"Processing Language: {i}".ljust(60), end="")
+        baseUrl = f"https://github.com/EvanLi/Github-Ranking/raw/master/Top100/{i}.md"
+        try:
+            ps = getData(baseUrl)
+        except Exception:
+            print("[FAILED]")
+            continue
         resultSet[i] = ps
-        print("[OK]")
+        end_time = time.perf_counter()
+        print(f"[OK] {end_time - start_time:.4f} seconds")
         if (rate_limit): time.sleep(interval)
     
-
+    print(f"Processing Language: Top 100 Stars".ljust(60), end="")
     start_time = time.perf_counter()
     baseUrl = f"https://github.com/EvanLi/Github-Ranking/raw/master/Top100/Top-100-stars.md"
-    ps = getData(baseUrl)
+    try:
+        ps = getData(baseUrl)
+    except Exception as e:
+        print("[FAILED]")
+        ps = None
     resultSet["TopStars"] = ps
     end_time = time.perf_counter()
-    print(f"[OK] {end_time - start_time:.4f} seconds")
+    if ps != []: print(f"[OK] {end_time - start_time:.4f} seconds")
 
-    print(f"Processing Language: {i}".ljust(60), end="")
+    print(f"Processing Language: Top 100 Forks".ljust(60), end="")
     start_time = time.perf_counter()
     baseUrl = f"https://github.com/EvanLi/Github-Ranking/raw/master/Top100/Top-100-forks.md"
-    ps = getData(baseUrl)
+    try:
+        ps = getData(baseUrl)
+    except Exception as e:
+        print("[FAILED]")
+        ps = None
     resultSet["TopForks"] = ps
     end_time = time.perf_counter()
-    print(f"[OK] {end_time - start_time:.4f} seconds")
+    if ps != []: print(f"[OK] {end_time - start_time:.4f} seconds")
 
     for k, v in resultSet.items():
-        if not v:
+        if v is None: 
+            print("[!] Wrong data.")
             continue
-        pandas.DataFrame(v, index=False).to_csv(f"{RES_DIR}\\{i.lower}.json")
-        with open(f"{RES_DIR}\\{i.lower}.json", "w") as fp: 
+        pandas.DataFrame(v).to_csv(f"{RES_DIR}\\{i.lower}.json", index=False)
+        with open(f"{RES_DIR}\\{k.lower}.json", "w") as fp: 
             json.dump(v, fp)
             fp.close()
 
